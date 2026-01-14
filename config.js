c98adc8804533a52e0da4c3ed6c1262d18b6b37a
Scott <n0hot@users.noreply.github.com>
2026-01-13 19:54:15 -0600

    Update config.js my changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const disableSetup = false;
-var topBarCenterText = `VA3HDL - FN04ga`;
+var topBarCenterText = `N0HOT - EN43es`;
 
 // Grid layout
 var layout_cols = 4;
@@ -10,44 +10,59 @@ var layout_rows = 3;
 // The values are [color code, menu text, target link, scale factor, side],
 // add new lines following the structure for extra menu options. The comma at the end is important!
 var aURL = [
-  ["f3de21ff", "satellite.js"],
-  ["2196F3", "CLUBLOG", "https://clublog.org/livestream/VA3HDL", "1.7"],
+//
+//  LEFT SIDE
+//  
   [
-    "2196F3",
-    "CONTEST",
-    "https://www.contestcalendar.com/fivewkcal.html",
-    "1",
-  ],
-  ["2196F3", "DX CLUSTER", "https://dxcluster.ha8tks.hu/map/", "1"],
-  [
-    "2196F3",
-    "LIGHTNING",
-    "https://map.blitzortung.org/#3.87/36.5/-89.41",
-    "1",
-    "R",
-  ],
-  ["2196F3", "PISTAR", "http://pi-star.local/", "1.2"],
-  [
-    "2196F3",
-    "RADAR",
-    "https://weather.gc.ca/?layers=alert,radar&center=43.39961001,-78.53212031&zoom=6&alertTableFilterProv=ON",
-    "1",
-    "R"
-  ],
-  ["2196F3", "TIME.IS", "https://time.is/", "1", "R"],
-  [
-    "2196F3",
-    "WEATHER",
-    "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=44.0157&lon=-79.4591&zoom=5",
-    "1",
-    "R",
+    "f3de21ff",
+    "satellite.js",
   ],
   [
-    "2196F3",
-    "WINDS",
-    "https://earth.nullschool.net/#current/wind/surface/level/orthographic=-78.79,44.09,3000",
-    "1",
-    "R",
+    "2196F3", "TIME.IS",
+    "https://time.is/", "1",
+  ],
+  [
+    "2196F3", "CLUBLOG",
+    "https://clublog.org/livestream/N0HOT", "1.5",
+  ],
+  [
+    "2196F3", "CONTEST",
+    "https://www.contestcalendar.com/fivewkcal.html", "1",
+  ],
+  [
+    "2196F3", "DX CLUSTER",
+    "https://dxcluster.ha8tks.hu/map/", "1.2",
+  ],
+  [
+    "2196F3", "POTA",
+    "https://pota.app/embed", "1",
+  ],
+//
+//  RIGHT SIDE
+//  
+  [
+    "2196F3", "ADSB",
+    "https://globe.adsbexchange.com", "1", "R",
+  ],
+  [
+    "2196F3", "LIGHTNING",
+    "https://map.blitzortung.org/#10.91/43.7763/-91.5206", "1", "R",
+  ],
+  [
+    "2196F3", "MUF",
+    "https://muf.hb9vqq.ch/", "1", "R",
+  ],
+  [
+    "2196F3", "Aurora Map",
+    "https://www.northernlightsmap.com/#map-section", "1", "R",
+  ],
+  [
+    "2196F3", "WEATHER",
+    "https://www.ventusky.com/#p=43.55;-91.71;7", "1", "R",
+  ],
+  [
+    "2196F3", "WINDS",
+    "https://earth.nullschool.net/#current/wind/surface/level/orthographic=-99.98,41.55,922/loc=-91.869,43.672", "1", "R",
   ],
 ];
 
@@ -58,43 +73,54 @@ var aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 var aIMG = [
-  ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
+  [
+    "USA RADAR",
+    "https://radar.weather.gov/ridge/standard/CONUS_loop.gif",
+  ],
   [
     "LOCAL RADAR",
     "https://radar.weather.gov/ridge/standard/KARX_loop.gif",
   ],
   [
     "NOAA D-RAP",
-    "https://services.swpc.noaa.gov/images/animations/d-rap/global/d-rap/latest.png",
+    "https://services.swpc.noaa.gov/images/animations/d-rap/global/latest.png",
   ],
   [
     "ISS POSITION",
     "https://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544",
   ],
   [
-    "SATELLITE CAN",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/can/EXTENT3/GOES16-CAN-EXTENT3-1125x560.gif",
+    "MUF",
+    "iframe|https://muf.hb9vqq.ch/",
   ],
   [
-    "SATELLITE CGL",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/cgl/EXTENT3/GOES16-CGL-EXTENT3-600x600.gif",
+    "SATELLITE UMV",
+    "https://cdn.star.nesdis.noaa.gov/GOES19/GLM/SECTOR/umv/EXTENT3/GOES19-UMV-EXTENT3-600x600.gif",
   ],
   [
     "LIGHTNING",
     "https://images.lightningmaps.org/blitzortung/america/index.php?animation=usa",
   ],
   [
-    "LIGHTNING LOCAL",
-    "https://www.blitzortung.org/en/Images/image_b_ny.png",
+    "HF Prop EN43",
+    "iframe|https://dxlook.com/?filter=grid&grid=EN43",
   ],
-  ["YOUTUBE EXAMPLE", "iframe|https://www.youtube.com/embed/fzPFaXAV_2Y?autoplay=1&mute=1"],
   [
-    "WEBSITE EXAMPLE",
-    "iframe|https://globe.adsbexchange.com/?airport=YYZ",
+    "LIGHTNING",
+    "https://images.lightningmaps.org/blitzortung/america/index.php?animation=usa",
   ],
-  ["VIDEO EXAMPLE", "https://himawari8.nict.go.jp/movie/720/20240611_pifd.mp4"],
-  ["HF PROPAGATION",
-    "https://www.hamqsl.com/solar101vhf.php"],
+  [
+    "Weather Local",
+    "https://zoom.earth/maps/satellite-hd/#view=43.77,-91.584,6z/date=2026-01-11,pm",
+  ],
+  [
+    "",
+    "https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png",
+  ],
+  [
+    "HAM Bands",
+    "https://www.arrl.org/images/view//Charts/Band_Chart_Image_for_ARRL_Web.jpg",
+  ],
 ];
 
 // Image rotation intervals in milliseconds per tile - If the line below is commented, all tiles will be rotated every 30000 milliseconds (30s)
