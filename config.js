226acf0562ba651df179de578bf016dc94ac2f97
Scott <n0hot@users.noreply.github.com>
2026-02-09 14:39:00 -0600

    Update config.js add open ham clock

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ var aURL = [
   ["2196F3", "Club Log", "https://clublog.org/livestream/N0HOT", "1.4"],
   ["2196F3", "CONTEST", "https://www.contestcalendar.com", "1"],
   ["2196F3", "DX CLUSTER", "https://dxcluster.ha8tks.hu/map/", "1"],
+  ["2196F3", "Ham Clock", "https://openhamclock.com", "1"],
   ["2196F3", "Ham DX", "https://dxmap.f5uii.net", "1"],
   ["2196F3", "Ham Sonde", "https://amateur.sondehub.org/#!mt=Mapnik&mz=5&qm=12h&mc=43.77017,-91.58347", "1"],
   ["2196F3", "Int Cluster", "https://holycluster.iarc.org/", "1"],
